667c1fd02ecb009b4edce4cd64ec7cc2967864c2
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-06-03 18:34:33 +0800

    added ix

--- include/text_draw.hpp
+++ include/text_draw.hpp
@@ -11,23 +11,20 @@ namespace g80 {
     
 
     using namespace std::placeholders;
-    using int_type = int16_t;
     
-    using expression_map = std::map<std::string, std::function<void(const std::string &, const int_type)>>;
+    using expression_map = std::map<std::string, std::function<void(const std::string &, const uint16_t)>>;
     
-    // Function Signatures:
-    // [direction] [length-int]
-
-    auto right(const std::string &command, const int_type i) -> void {
+    auto right(const std::string &command, const uint16_t i) -> void {
     }
-    auto catch_all(const std::string &command, const int_type i) -> void {
+    auto catch_all(const std::string &command, const uint16_t i) -> void {
     }
 
     class text_draw {
     public:
-        text_draw(int_type width, int_type height) : 
+        text_draw(const uint16_t width, const uint16_t height) : 
         width_(width),
         height_(height),
+        size_(width_ * height_),
         buffer_ch_(width_ * height_, ch_),
         buffer_col_(width_ * height_, col_) {
             expression_map_["x"] = std::bind(catch_all, _1, _2);
@@ -42,19 +39,27 @@ namespace g80 {
             expression_map_["ur"] = std::bind(catch_all, _1, _2);
             expression_map_["ll"] = std::bind(catch_all, _1, _2);
             expression_map_["lr"] = std::bind(catch_all, _1, _2);
+            expression_map_["arc"] = std::bind(catch_all, _1, _2);
+            expression_map_["fil"] = std::bind(catch_all, _1, _2);
+            expression_map_["t"] = std::bind(catch_all, _1, _2);
+            expression_map_["ctx"] = std::bind(catch_all, _1, _2);
+            expression_map_["cty"] = std::bind(catch_all, _1, _2);
 
         }
 
+        inline auto ix(const uint16_t x, const uint16_t y) const -> const uint16_t {
+            return y * width_ + x;
+        }
+
     private:
-        int_type x_{0}, y_{0};
-        char ch_{' '};
+        bool wrap_around_ = true;
+        uint8_t ch_{32};
         uint8_t col_{7};
-        int_type width_, height_;
-        std::vector<char> buffer_ch_;
+        uint16_t x_{0}, y_{0};
+        uint16_t width_, height_, size_;
+        std::vector<uint8_t> buffer_ch_;
         std::vector<uint8_t> buffer_col_;
         expression_map expression_map_;
-        
-
     };
 }
 #endif 
