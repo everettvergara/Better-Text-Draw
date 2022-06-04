22ff5d7fe40dc80e858c59ef3f0e6898591d883b
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-06-04 20:06:57 +0800

    misc edits

--- btd.cpp
+++ btd.cpp
@@ -5,7 +5,7 @@
 using namespace g80;
 auto main(const int argc, const char *argv[]) -> int {
     better_text_draw btd(100, 10);
-    btd.eval("ch*r10");
+    btd.eval("    ch * r10");
     btd.show();
 }
 
