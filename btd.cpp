b6670af489fc3c8e70ef83e26e4d4a907aa13c26
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-06-05 21:04:23 +0800

    added validations

--- btd.cpp
+++ btd.cpp
@@ -1,10 +1,55 @@
+/*
+ *  Main program for processing
+ *  Copyright (C) 2022 Everett Gaius S. Vergara
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ */
+
 #include <iostream>
-#include <map>
 #include "better_text_draw.hpp"
 
 using namespace g80;
+
+auto validate(const int argc, const char *argv[]) -> std::string {
+
+    static std::string format = 
+    "\n\n"
+    "Syntax:\n"
+    "btd {width} {height} < command_input_file\n";
+
+    auto is_number = [&](const std::string &to_check) -> bool {
+        for (auto &ch : to_check) if (!std::isdigit(ch)) return false;
+        return true;
+    };
+
+    if (argc < 3) return std::string("Must have width and height of the canvass.") + format;    
+    if (!is_number(argv[1])) return std::string("Please input a valid width for the canvass") + format;
+    if (!is_number(argv[2])) return std::string("Please input a valid height for the canvass") + format;
+    if (std::stoi(argv[1]) <= 0) return std::string("Valid width for canvass > 0") + format;
+    if (std::stoi(argv[2]) <= 0) return std::string("Valid height for canvass > 0") + format;
+
+    return "";
+}
+
 auto main(const int argc, const char *argv[]) -> int {
     
+    if (auto msg = validate(argc, argv); msg.size() > 0) {
+        std::cout << msg << std::endl;
+        return -1;
+    }
+
     try {
         better_text_draw btd(130, 30, ' ', 7);
         btd.eval(
